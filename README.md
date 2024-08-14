b5ce48f91e7b7df40a0fb06c88101f4dda3d33f9
null crafter <163591881+null-crafter@users.noreply.github.com>
2024-08-14 01:28:00 +0100

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# BSc CS Final Project Prototype
---
+++ src/core/urls.py
@@ -0,0 +1,30 @@
+from django.urls import include, path, re_path
+from rest_framework.authtoken import views as authtoken_views
+
+from . import views
+
+app_name = "core"
+api_urlpatterns = [
+    path("get-token/", authtoken_views.obtain_auth_token),
+    path("channel-info/", views.ChannelInfoView.as_view(), name="channel_info"),
+    path(
+        "connected-clients/",
+        views.ConnectedClientsView.as_view(),
+        name="connected_clients",
+    ),
+    path(
+        "channel-messages/",
+        views.ChannelMessageView.as_view(),
+        name="channel_messages",
+    ),
+    path(
+        "send-message/",
+        views.SendMessageView.as_view(),
+        name="send_message",
+    ),
+]
+
+urlpatterns = [
+    path("api/", include(api_urlpatterns)),
+    re_path(r"^(?P<path>.*)$", views.FrontendView.as_view()),
+]
